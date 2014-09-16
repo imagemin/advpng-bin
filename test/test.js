416c2314ea094a644810147bd8bba35f3e837636
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-16 14:28:10 +0200

    Rewrite

--- test/test.js
+++ test/test.js
@@ -10,53 +10,53 @@ var path = require('path');
 var rm = require('rimraf');
 
 describe('advpng()', function () {
-  afterEach(function (cb) {
-    rm(path.join(__dirname, 'tmp'), cb);
-  });
+	afterEach(function (cb) {
+		rm(path.join(__dirname, 'tmp'), cb);
+	});
 
-  beforeEach(function () {
-    fs.mkdirSync(path.join(__dirname, 'tmp'));
-  });
+	beforeEach(function () {
+		fs.mkdirSync(path.join(__dirname, 'tmp'));
+	});
 
-  it('should rebuild the advpng binaries', function (cb) {
-    var tmp = path.join(__dirname, 'tmp');
-    var builder = new BinBuild()
-      .src('http://prdownloads.sourceforge.net/advancemame/advancecomp-1.19.tar.gz')
-      .cfg('./configure --prefix="' + tmp + '" --bindir="' + tmp + '"')
-      .make('make install');
+	it('should rebuild the advpng binaries', function (cb) {
+		var tmp = path.join(__dirname, 'tmp');
+		var builder = new BinBuild()
+			.src('http://prdownloads.sourceforge.net/advancemame/advancecomp-1.19.tar.gz')
+			.cfg('./configure --prefix="' + tmp + '" --bindir="' + tmp + '"')
+			.make('make install');
 
-    builder.build(function (err) {
-      assert(!err);
-      assert(fs.existsSync(path.join(tmp, 'advpng')));
-      cb();
-    });
-  });
+		builder.build(function (err) {
+			assert(!err);
+			assert(fs.existsSync(path.join(tmp, 'advpng')));
+			cb();
+		});
+	});
 
-  it('should return path to binary and verify that it is working', function (cb) {
-    var binPath = require('../').path;
+	it('should return path to binary and verify that it is working', function (cb) {
+		var binPath = require('../').path;
 
-    binCheck(binPath, ['--version'], function (err, works) {
-      cb(assert.equal(works, true));
-    });
-  });
+		binCheck(binPath, ['--version'], function (err, works) {
+			cb(assert.equal(works, true));
+		});
+	});
 
-  it('should minify a PNG', function (cb) {
-    var src = path.join(__dirname, 'fixtures/test.png');
-    var dest = path.join(__dirname, 'tmp/test.png');
-    var binPath = require('../').path;
-    var args = [
-      '--recompress',
-      '--shrink-extra',
-      path.join(__dirname, 'tmp/test.png')
-    ];
+	it('should minify a PNG', function (cb) {
+		var src = path.join(__dirname, 'fixtures/test.png');
+		var dest = path.join(__dirname, 'tmp/test.png');
+		var binPath = require('../').path;
+		var args = [
+			'--recompress',
+			'--shrink-extra',
+			path.join(__dirname, 'tmp/test.png')
+		];
 
-    fs.writeFileSync(dest, fs.readFileSync(src));
+		fs.writeFileSync(dest, fs.readFileSync(src));
 
-    execFile(binPath, args, function () {
-      var src = fs.statSync(path.join(__dirname, 'fixtures/test.png')).size;
-      var dest = fs.statSync(path.join(__dirname, 'tmp/test.png')).size;
+		execFile(binPath, args, function () {
+			var src = fs.statSync(path.join(__dirname, 'fixtures/test.png')).size;
+			var dest = fs.statSync(path.join(__dirname, 'tmp/test.png')).size;
 
-      cb(assert(dest < src));
-    });
-  });
+			cb(assert(dest < src));
+		});
+	});
 });
